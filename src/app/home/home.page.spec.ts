82e7f5959895231ed303112704990b2c1944a1e6
viriansyah <72482988+viriansyah@users.noreply.github.com>
2022-12-14 11:51:31 +0700

    ToDoList

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,24 +1,53 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { AddNewTaskPage } from '../add-new-task/add-new-task.page';
+import { TodoService } from '../todo.service';
+import { UpdateTaskPage } from '../update-task/update-task.page';
 
-import { HomePage } from './home.page';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  todoList = []
+  
+  today: number = Date.now();
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+  constructor(public modalCtlr: ModalController, public todoService:TodoService) { 
+    this.getAllTask()
+  }
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  async addNewItem() {
+    const modal = await this.modalCtlr.create({
+      component: AddNewTaskPage,
+    })
+    modal.onDidDismiss().then(newTask =>{
+      this.getAllTask()
+    })
+    return await modal.present()
+  }
 
-    fixture = TestBed.createComponent(HomePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  getAllTask(){
+    this.todoList = this.todoService.getAllTasks()
+    console.log(this.todoService.getAllTasks());
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  delete(key) { 
+    this.todoService.deleteTask(key)
+    this.getAllTask()
+  }
+
+  async update(selectedTask){
+    const modal = await this.modalCtlr.create({
+      component: UpdateTaskPage,
+      componentProps: {task: selectedTask}
+    })
+
+    modal.onDidDismiss().then(()=>{
+      this.getAllTask()
+    })
+    
+    return await modal.present()
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddNewTaskPage } from '../add-new-task/add-new-task.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  todoList =[{
+    itemName : 'Coding',
+    itemDueDate: '01-13-21',
+    itemPriority: 'high',
+    itemCategory : "work"
+  },
+  {
+    itemName : 'Design',
+    itemDueDate: '02-17-21',
+    itemPriority: 'low',
+    itemCategory : "work"
+  },
+  {
+    itemName : 'Shopping',
+    itemDueDate: '01-02-21',
+    itemPriority: 'middle',
+    itemCategory : "personal"
+  },
+  {
+    itemName : 'Workout',
+    itemDueDate: '01-22-21',
+    itemPriority: 'high',
+    itemCategory : "personal"
+  }
+]
+
+  today: number = Date.now()
+
+  constructor(public modalCtrl: ModalController) {}
+
+  async addTask(){
+    const modal= await this.modalCtrl.create({
+      component: AddNewTaskPage
+    })
+    return await modal.present
+  }
 
 }
